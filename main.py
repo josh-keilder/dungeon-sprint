06dd58edcbdffc63a5478c96007fbd7a16aaada8
Joshua Blasiman <joshuablasiman@gmail.com>
2025-08-30 20:39:00 -0400

    Updated main.py |

--- main.py
+++ main.py
@@ -1,18 +1,18 @@
 import pygame
 import sys
-import os
 
-from player import Player
+from settings import *
+from player import *
 from stateManager import Level, Start, GameStateManager
-
-FPS = 60
 
 
 class Game:
     def __init__(self):
         pygame.init()
-        self.screen = pygame.display.set_mode((1280, 720))
+        self.screen = pygame.display.set_mode((SCREENWIDTH, SCREENHEIGHT))
         self.clock = pygame.time.Clock()
+
+        self.running = True
         
         self.gameStateManager = GameStateManager('start')
         self.start = Start(self.screen, self.gameStateManager)
@@ -20,24 +20,23 @@ class Game:
         
 
         self.states = {'start':self.start, 'level':self.level}
-
     def run(self):
-        while True:
-            for event in pygame.event.get():
+            while self.running:
+                 self.update()
+                 self.draw()
+    def update(self):
+        for event in pygame.event.get():
                 if event.type == pygame.QUIT:
-                    pygame.quit()
-                    sys.exit()
+                    self.running = False
 
-
-
-            # inside of the self.states dictionary, look for the key that is returned from the get_state() method and run it
-            self.states[self.gameStateManager.get_state()].run() 
-
-            pygame.display.update()
-            self.clock.tick(FPS)
-
-
-
+        pygame.display.update()
+        self.clock.tick(FRAMERATE)
+    def draw(self):
+        # inside of the self.states dictionary, look for the key that is returned from the get_state() method and run it
+        self.states[self.gameStateManager.get_state()].run()
+    def close(self):
+        pygame.quit()
+        sys.exit()
 
 if __name__ == '__main__':
     game = Game()
