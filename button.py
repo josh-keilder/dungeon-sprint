0b8e930ec2968af79f2e9d5254663578cd268f1e
Keilder <joshuablasiman@gmail.com>
2025-10-06 21:29:40 -0400

    Added outlines for the buttons

--- button.py
+++ button.py
@@ -8,10 +8,23 @@ class Button():
         self.rect.topleft = (pos_x,pos_y)
         self.screen = screen
         self.clicked = False
+        self.mask = pygame.mask.from_surface(self.image)
 
     def draw(self):
         # puts the buttons on screen at the rect topleft coordinates
         self.screen.blit(self.image, (self.rect.x, self.rect.y))
+
+    def button_outline(self, img, pos):
+        mask = pygame.mask.from_surface(img)
+        mask_outline = mask.outline()
+        mask_surf = pygame.Surface(img.get_size())
+        for pixel in mask_outline:
+            mask_surf.set_at(pixel,(255,255,255))
+        mask_surf.set_colorkey((0,0,0))
+        self.screen.blit(mask_surf,(pos[0] - 1, pos[1]))
+        self.screen.blit(mask_surf,(pos[0] + 1, pos[1]))
+        self.screen.blit(mask_surf,(pos[0], pos[1] - 1))
+        self.screen.blit(mask_surf,(pos[0], pos[1] + 1))
 
     def is_clicked(self) -> bool:
         # gets mouse position
@@ -19,6 +32,7 @@ class Button():
         action = False
         # checks if mouse is over and clicked the button
         if self.rect.collidepoint(pos):
+            self.button_outline(self.image, (self.rect.x, self.rect.y))
             if pygame.mouse.get_pressed()[0] == 1 and self.clicked == False:
                 self.clicked = True
                 action = True       
