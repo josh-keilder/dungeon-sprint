55e8b8ce85bb77d289f98d91fd11c806d222c483
Keilder <joshuablasiman@gmail.com>
2025-10-06 21:37:17 -0400

    Added button outlines

--- button.py
+++ button.py
@@ -1,6 +1,8 @@
 import pygame
 from settings import *
 
+pygame.mixer.init()
+
 class Button():
     def __init__(self, screen, image, pos_x, pos_y):
         self.image = image
@@ -30,9 +32,10 @@ class Button():
         # gets mouse position
         pos = pygame.mouse.get_pos()
         action = False
-        # checks if mouse is over and clicked the button
+        # checks if mouse is over the button, draws the outline and checks if player clicked the button
         if self.rect.collidepoint(pos):
             self.button_outline(self.image, (self.rect.x, self.rect.y))
+            pygame.mixer.music.load("Assets/Sounds/Button_Hover")
             if pygame.mouse.get_pressed()[0] == 1 and self.clicked == False:
                 self.clicked = True
                 action = True       
